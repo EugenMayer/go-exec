0e35f66dc929ee08d7d87ab42f657577600ee06e
em <eugen.mayer@kontextwork.de>
2018-10-29 14:53:39 +0100

    path from remote

--- runner/cp_from_remote_runner.go
+++ runner/cp_from_remote_runner.go
@@ -2,7 +2,6 @@ package runner
 
 import (
 	"github.com/eugenmayer/go-sshclient/sshwrapper"
-	"os"
 )
 
 type CopyFromRemoteToLocalRunner struct {
@@ -11,24 +10,6 @@ type CopyFromRemoteToLocalRunner struct {
 }
 
 func (runner CopyFromRemoteToLocalRunner) Copy(source string, dest string) (stdOut string, stdErr string, err error) {
-	runner.SshApi.Connect()
-
-	if session, err := runner.SshApi.Client.NewSession(); err != nil {
-		return "", "", err
-	} else {
-		runner.SshApi.Session = session
-	}
-
-	r, err := runner.SshApi.Session.Output("dd if=" + source)
-	if err != nil {
-		return "", "", err
-	}
-	defer runner.SshApi.Session.Close()
-	file, err := os.Create(dest)
-	if err != nil {
-		return "", "", err
-	}
-	//write to local file
-	file.Write(r)
-	return "", "", nil
+	err = runner.SshApi.CopyFromRemote(source, dest)
+	return runner.SshApi.GetStdOut(), runner.SshApi.GetStdErr(), err
 }
--- runner/cp_to_remote_runner.go
+++ runner/cp_to_remote_runner.go
@@ -12,6 +12,5 @@ type CopyToRemoteFromLocalRunner struct {
 func (runner CopyToRemoteFromLocalRunner) Copy(source string, dest string) (stdOut string, stdErr string, err error) {
 	runner.SshApi.ConnectAndSession()
 	err = runner.SshApi.CopyToRemote(source, dest)
-	runner.SshApi.Session.Close()
 	return "", "", err
 }
