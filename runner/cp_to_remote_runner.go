eedf5ed226c011533c2b745a78515c43116cd6a8
em <eugen.mayer@kontextwork.de>
2018-10-29 15:12:39 +0100

    do no open a custom session

--- runner/cp_to_remote_runner.go
+++ runner/cp_to_remote_runner.go
@@ -10,7 +10,6 @@ type CopyToRemoteFromLocalRunner struct {
 }
 
 func (runner CopyToRemoteFromLocalRunner) Copy(source string, dest string) (stdOut string, stdErr string, err error) {
-	runner.SshApi.ConnectAndSession()
 	err = runner.SshApi.CopyToRemote(source, dest)
 	return "", "", err
 }
