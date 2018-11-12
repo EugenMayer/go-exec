129d1add9ed4c89d5c07486369fa36ef4c5c6002
em <eugen.mayer@kontextwork.de>
2018-11-12 14:20:10 +0100

    fix docker runner

--- runner/command_docker_runner.go
+++ runner/command_docker_runner.go
@@ -12,7 +12,7 @@ type DockerCommandRunner struct {
 
 func (runner DockerCommandRunner) Run(cmdStr string) (stdOut string, stdErr string, err error) {
 	if runner.Verbose {
-		fmt.Println(fmt.Sprintf("Using SSH. Running on server: %s. Running command: %s", runner.SshApi.Host, cmdStr))
+		fmt.Println(fmt.Sprintf("Using Docker. Running on container: %s. Running command: %s", runner.Container, cmdStr))
 	}
-	return exec.Run(fmt.Sprint("%docker exec %s %s", runner.Container, cmdStr), runner.Verbose)
+	return exec.Run(fmt.Sprintf("docker exec %s %s", runner.Container, cmdStr), runner.Verbose)
 }
