ec265e872610bf5136bfd5df1f3ca3a76e9f23c4
em <eugen.mayer@kontextwork.de>
2021-09-06 10:43:12 +0200

    Fix implementation of Start

--- exec/shell.go
+++ exec/shell.go
@@ -1,38 +1,45 @@
 package exec
 
 import (
-	"bytes"
+	"bufio"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
 )
 
 // Start a command. You will need to call .run() yourself. Use this for non-blocking execution like live error streaming.
-func Start(cmdStr string, workDir string, verbose bool) (*exec.Cmd, error) {
+func Start(cmdStr string, workDir string, verbose bool) (cmd *exec.Cmd, stdOut io.ReadCloser, stdErr io.ReadCloser, err error ) {
 	if verbose {
 		fmt.Println(fmt.Sprintf("Running: %s", cmdStr))
 	}
 
 	// Using this variant as we have pipes or stdout redirect at some point
-	cmd := exec.Command("sh", "-c", cmdStr)
-	err := cmd.Start()
+	cmd = exec.Command("sh", "-c", cmdStr)
 
 	if err != nil {
-		return nil, nil
+		return nil, nil,nil, err
 	}
 
 	// since we use dash, we could have a very basic env without proper paths. Ensure we at least have the usual extension point
 	// for extra binaries
 	cmd.Env = append(os.Environ(), "PATH="+os.Getenv("PATH")+":/usr/local/bin")
 	cmd.Dir = workDir
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
+	stdOut, err = cmd.StdoutPipe()
+	if err != nil {
+		return nil, nil,nil, err
+	}
 
-	return cmd, err
+	stdErr, err = cmd.StderrPipe()
+	if err != nil {
+		return nil, nil,nil, err
+	}
+
+	err = cmd.Start()
+
+	return cmd, stdOut, stdErr, nil
 }
 
 // Run a command right away
@@ -42,22 +49,21 @@ func Run(cmdStr string, verbose bool) (stdOut string, stdErr string, err error) 
 
 // RunInDir RunDir a command in a specific working directory
 func RunInDir(cmdStr string, workingDir string, verbose bool) (stdOut string, stdErr string, err error) {
-	var cmd, startErr = Start(cmdStr, workingDir, verbose)
+	var cmd, stdOutStream, stdErrStream, startErr = Start(cmdStr, workingDir, verbose)
 	if startErr != nil {
 		return "", "", startErr
 	}
 
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
+	stdOut = readBuffer(stdOutStream)
+	stdErr = readBuffer(stdErrStream)
+
 	err = cmd.Run()
 
 	if err != nil {
-		err = errors.New(err.Error() + "\n" + stderr.String())
+		err = errors.New(err.Error() + "\n" + stdErr)
 	}
 
-	return stdout.String(), stderr.String(), err
+	return stdOut, stdErr, err
 }
 
 // RunFatal Run a command and fatal if an error occurs ( non exit 0 )
@@ -68,4 +74,15 @@ func RunFatal(cmdStr string, verbose bool) (stdOut string, stdErr string) {
 	}
 
 	return
+}
+
+func readBuffer(buffer io.ReadCloser) string {
+	scanner := bufio.NewScanner(buffer)
+	scanner.Split(bufio.ScanWords)
+	var out = ""
+	for scanner.Scan() {
+		m := scanner.Text()
+		out = out + fmt.Sprintln(m)
+	}
+	return out
 }
