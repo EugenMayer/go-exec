95a70df0091527b0006b8a64363928a141e338a7
em <eugen.mayer@kontextwork.de>
2021-09-06 09:53:01 +0200

    add non blocking variant Start to exec

--- exec/shell.go
+++ exec/shell.go
@@ -9,23 +9,44 @@ import (
 	"os/exec"
 )
 
-// Run a command right away
-func Run(cmdStr string, verbose bool) (stdOut string, stdErr string, err error) {
-	return RunInDir(cmdStr, "", verbose)
-}
-
-// RunDir a command in a specific working directory
-func RunInDir(cmdStr string, workingDir string, verbose bool) (stdOut string, stdErr string, err error) {
+// Start a command. You will need to call .run() yourself. Use this for non-blocking execution like live error streaming.
+func Start(cmdStr string, workDir string, verbose bool) (*exec.Cmd, error) {
 	if verbose {
 		fmt.Println(fmt.Sprintf("Running: %s", cmdStr))
 	}
 
 	// Using this variant as we have pipes or stdout redirect at some point
 	cmd := exec.Command("sh", "-c", cmdStr)
+	err := cmd.Start()
+
+	if err != nil {
+		return nil, nil
+	}
+
 	// since we use dash, we could have a very basic env without proper paths. Ensure we at least have the usual extension point
 	// for extra binaries
 	cmd.Env = append(os.Environ(), "PATH="+os.Getenv("PATH")+":/usr/local/bin")
-	cmd.Dir = workingDir
+	cmd.Dir = workDir
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	return cmd, err
+}
+
+// Run a command right away
+func Run(cmdStr string, verbose bool) (stdOut string, stdErr string, err error) {
+	return RunInDir(cmdStr, "", verbose)
+}
+
+// RunInDir RunDir a command in a specific working directory
+func RunInDir(cmdStr string, workingDir string, verbose bool) (stdOut string, stdErr string, err error) {
+	var cmd, startErr = Start(cmdStr, workingDir, verbose)
+	if startErr != nil {
+		return "", "", startErr
+	}
+
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &stdout
@@ -39,7 +60,7 @@ func RunInDir(cmdStr string, workingDir string, verbose bool) (stdOut string, st
 	return stdout.String(), stderr.String(), err
 }
 
-// Run a command and fatal if an error occurs ( non exit 0 )
+// RunFatal Run a command and fatal if an error occurs ( non exit 0 )
 func RunFatal(cmdStr string, verbose bool) (stdOut string, stdErr string) {
 	stdOut,stdErr, err := Run(cmdStr, verbose)
 	if err != nil {
